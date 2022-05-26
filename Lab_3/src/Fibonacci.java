4222e4aea0c8ef53eebbe8d5f81cf84ed455c244
kdanking17 <63831976+kdanking17@users.noreply.github.com>
2022-05-26 15:17:44 -0600

    update code

--- Lab_3/src/Fibonacci.java
+++ Lab_3/src/Fibonacci.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Fibonacci {
+    public static int fibonacci(int n){
+        if(n == 0) {
+            return 0;
+        }
+        else if (n == 1) {
+            return 1;
+        }
+        else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Fibonacci Sequence index:");
+        int n = in.nextInt();
+
+        System.out.println(fibonacci(n));
 
     }
 }
